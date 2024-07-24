efd4f13936f02d9820a074d4b3233ee1b4eb89dd
James Clarke <164679524+jjjclarke@users.noreply.github.com>
2024-07-24 18:16:46 +0100

    Rename write_md_file to write_html_file

--- markdown-py/file_io.py
+++ markdown-py/file_io.py
@@ -2,6 +2,6 @@ def read_md_file(file_path):
     with open(file_path, "r") as file:
         return file.read()
 
-def write_md_file(file_path, contents):
+def write_html_file(file_path, contents):
     with open(file_path, "w") as file:
         file.write(contents)
--- markdown-py/markdown.py
+++ markdown-py/markdown.py
@@ -1,11 +1,11 @@
 import sys
 from converter import markdown_to_html
-from file_io import read_md_file, write_md_file
+from file_io import read_md_file, write_html_file
 
 def main(input_file_name, output_file_name):
     markdown_text = read_md_file(input_file_name)
     html_text = markdown_to_html(markdown_text)
-    write_md_file(output_file_name, html_text) # should be called write_html_file
+    write_html_file(output_file_name, html_text)
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
